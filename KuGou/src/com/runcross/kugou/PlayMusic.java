d3a2949a59e43e82f815b09cefee1ceae75c5f22
RunCross <rujiang_g@qq.com>
2013-11-18 21:56:22 +0800

    kugou

--- KuGou/src/com/runcross/kugou/PlayMusic.java
+++ KuGou/src/com/runcross/kugou/PlayMusic.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -16,12 +18,12 @@ import android.widget.TextView;
 public class PlayMusic extends Activity {
 
 	private ImageButton back;
-	private ImageButton songList;
-	private ImageButton songModel;
-	private ImageButton songPrev;
-	private ImageButton songMusic;
-	private ImageButton songNext;
-	private ImageButton songMore;
+	// private ImageButton songList;
+	 private ImageButton songModel;
+	// private ImageButton songPrev;
+	// private ImageButton songMusic;
+	// private ImageButton songNext;
+	// private ImageButton songMore;
 
 	private TextView songTitle;
 	private TextView songSonger;
@@ -30,58 +32,70 @@ public class PlayMusic extends Activity {
 
 	private SeekBar songProgress;
 	private Music music;
-	
+
 	private SeekBar songVolume;
-	
-	private int model;
+
+	private int modes[];
+	public static int modeFlag ;
 	private boolean isMore = false;
 
+	private LinearLayout progress;
+	private LinearLayout operation;
 	
+	public static int volume;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.play_music);
-		
+
 		music = (Music) getIntent().getExtras().get("music");
-		
+
 		initSub();
 
 		initTitle();
-		
+
 		initProgress();
-		
+
 		initVolume();
-		
+
 		SharedPreferences sp = getSharedPreferences("kugou", MODE_PRIVATE);
-		
-		
+		modeFlag = sp.getInt("mode", 0);
+
 	}
 
 	private void initVolume() {
 		songVolume.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
+
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
-				Intent intent = new Intent("com.runcross.kugou.volume");
+				Intent intent = new Intent("com.runcross.kugou.volumechange");
 				intent.putExtra("progress", progress);
 				sendBroadcast(intent);
 			}
 		});
 	}
 
-	public void onMusicClick(View view){
-		switch(view.getId()){
+	public void onMusicClick(View view) {
+		switch (view.getId()) {
 		case R.id.play_model:
+			modeFlag++;
+			if(modeFlag == modes.length){
+				modeFlag = 0;
+			}
+			Intent intentModel = new Intent("com.runcross.kugou.model");
+			intentModel.putExtra("progress", modeFlag);
+			sendBroadcast(intentModel);
+			songModel.setImageResource(modes[modeFlag]);
 			break;
 		case R.id.play_prev:
 			break;
@@ -91,22 +105,30 @@ public class PlayMusic extends Activity {
 			break;
 		case R.id.play_more:
 			break;
+		case R.id.play_title_back:
+			break;
+		case R.id.play_list:
+			break;
 		}
 	}
 
 	private void initProgress() {
 		timeBegin.setText("0:00");
-		timeEnd.setText(String.valueOf(music.getDuration()/60+":"+(music.getDuration()%60 >9?music.getDuration()%60:"0"+music.getDuration()%60)));
+		timeEnd.setText(String.valueOf(music.getDuration()
+				/ 60
+				+ ":"
+				+ (music.getDuration() % 60 > 9 ? music.getDuration() % 60
+						: "0" + music.getDuration() % 60)));
 		songProgress.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
+
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
@@ -119,24 +141,24 @@ public class PlayMusic extends Activity {
 
 	private void initTitle() {
 		back.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 			}
 		});
-		
+
 		songTitle.setText(music.getName());
 		songSonger.setText(music.getArtist());
 	}
 
 	private void initSub() {
 		back = (ImageButton) findViewById(R.id.play_title_back);
-		songList = (ImageButton) findViewById(R.id.play_list);
+//		songList = (ImageButton) findViewById(R.id.play_list);
 		songModel = (ImageButton) findViewById(R.id.play_model);
-		songPrev = (ImageButton) findViewById(R.id.play_prev);
-		songMusic = (ImageButton) findViewById(R.id.play_music);
-		songNext = (ImageButton) findViewById(R.id.play_next);
-		songMore = (ImageButton) findViewById(R.id.play_more);
+//		songPrev = (ImageButton) findViewById(R.id.play_prev);
+//		songMusic = (ImageButton) findViewById(R.id.play_music);
+//		songNext = (ImageButton) findViewById(R.id.play_next);
+//		songMore = (ImageButton) findViewById(R.id.play_more);
 
 		songTitle = (TextView) findViewById(R.id.song_title);
 		songSonger = (TextView) findViewById(R.id.song_songer);
@@ -146,5 +168,13 @@ public class PlayMusic extends Activity {
 		songProgress = (SeekBar) findViewById(R.id.play_progress);
 
 		songVolume = (SeekBar) findViewById(R.id.play_volume);
+		
+		progress = (LinearLayout) findViewById(R.id.play_progress_linear);
+		operation  = (LinearLayout) findViewById(R.id.play_opera_more);
+		
+		modes = new int[]{R.drawable.ic_player_mode_all_default,
+				R.drawable.ic_player_mode_random_default,
+				R.drawable.ic_player_mode_sequence_default,
+				R.drawable.ic_player_mode_single_default};
 	}
 }
